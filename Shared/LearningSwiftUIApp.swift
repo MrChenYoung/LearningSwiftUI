a2edbc65f20d4cbc7af86ce0f3e272ef01c0a4a8
chenhuiyi <chenhuiyiyoung@163.com>
2022-03-28 17:39:02 +0800

    Initial Commit

---
+++ Shared/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  Shared
+//
+//  Created by MrChen on 2022/3/28.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
